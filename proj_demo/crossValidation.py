307c33769e7a4d730cb9702b5b587f59912d9ba4
baiyutai <nxyczyt@163.com>
2017-12-16 17:07:36 +0800

    Update crossValidation.py
    [未经验证的代码]多抄了几行

--- proj_demo/crossValidation.py
+++ proj_demo/crossValidation.py
@@ -2,9 +2,17 @@
 
 # 采用20折交叉验证，将训练集1300划分为20个大小为65的子集，依然采用 Top5 准确率作为评估标准
 
-from train import train
-from evaluate import test
+import torch
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.optim.lr_scheduler import LambdaLR as LR_Policy
+
+import models
+import train as tra
+import evaluate as eva
 from dataset import VideoFeatDataset as dset
+from tools import utils
 
 filelist = []
 with open(eva.opt.video_flist, 'r') as rf:
@@ -29,52 +37,71 @@ for i in range(FoldNum):
 
 for iter in range(FoldNum):
     print('this is ' + str(iter) + 'th iter')
-
-    train_dataset = dset(tra.opt.data_dir, flist=tra.opt.flist)
-    train_dataset.pathlist = trainlist[iter]
-
-    print('number of train samples is: {0} % {1} times'.format(len(train_dataset),iter))
-    print('finished loading train data % {0} times'.format(iter))
-
-    test_video_dataset = dset(eva.opt.data_dir, eva.opt.video_flist, which_feat='vfeat')
-    test_audio_dataset = dset(eva.opt.data_dir, eva.opt.audio_flist, which_feat='afeat')
-    test_video_dataset.pathlist = testlist[iter];
-    test_audio_dataset.pathlist = testlist[iter];
-
-    print('number of test samples is: {0} % {1} times'.format(len(test_video_dataset),iter))
-    print('finished loading test data % {0} times'.format(iter))
-
-    global opt
+    
+    # train
     # create model
     model = models.VGGLikeFC()
 
-    if opt.init_model != '':
-        print('loading pretrained model from {0}'.format(opt.init_model))
-        model.load_state_dict(torch.load(opt.init_model))
+    if tra.opt.init_model != '':
+        print('loading pretrained model from {0}'.format(tra.opt.init_model))
+        model.load_state_dict(torch.load(tra.opt.init_model))
 
     # Contrastive Loss
     criterion = models.ContrastiveLoss()
 
-    if opt.cuda:
+    if tra.opt.cuda:
         print('shift model and criterion to GPU .. ')
         model = model.cuda()
         criterion = criterion.cuda()
 
+    train_dataset = dset(tra.opt.data_dir, flist=tra.opt.flist)
+    train_dataset.pathlist = tra[iter]
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=tra.opt.batchSize,
+                                              shuffle=True, num_workers=int(tra.opt.workers))
+
+    print('number of train samples is: {0} % {1} times'.format(len(train_dataset),iter))
+    print('finished loading train data % {0} times'.format(iter))
+    
     # new optimizer
     optimizer = optim.Adam(model.parameters(), weight_decay=opt.weight_decay)
 
-    for epoch in range(opt.max_epochs):
+    for epoch in range(tra.opt.max_epochs):
         #################################
         # train for one epoch
         #################################
-        train(train_loader, model, criterion, optimizer, epoch, opt)
+        tra.train(train_loader, model, criterion, optimizer, epoch, tra.opt)
         optimizer.step()
 
         ##################################
         # save checkpoint every 10 epochs
         ##################################
-        if ((epoch+1) % opt.epoch_save) == 0:
-            path_checkpoint = '{0}/{1}_{3}times_state_epoch{2}.pth'.format(opt.checkpoint_folder, opt.prefix, epoch+1, i+1)
+        if ((epoch+1) % tra.opt.epoch_save) == 0:
+            path_checkpoint = '{0}/{1}_{3}times_state_epoch{2}.pth'.format(tra.opt.checkpoint_folder, tra.opt.prefix, epoch+1, iter+1)
             utils.save_checkpoint(model.state_dict(), path_checkpoint)
-            
-    test(test_video_loader, test_audio_loader, model, opt)
+
+    # test
+    test_video_dataset = dset(eva.opt.data_dir, eva.opt.video_flist, which_feat='vfeat')
+    test_audio_dataset = dset(eva.opt.data_dir, eva.opt.audio_flist, which_feat='afeat')
+    test_video_dataset.pathlist = testlist[iter];
+    test_audio_dataset.pathlist = testlist[iter];
+    test_video_loader = torch.utils.data.DataLoader(test_video_dataset,
+                                                    batch_size=eva.opt.batchSize,
+                                                    shuffle=False, 
+                                                    num_workers=int(opt.workers))
+    test_audio_loader = torch.utils.data.DataLoader(test_audio_dataset, 
+                                                    batch_size=eva.opt.batchSize,
+                                                    shuffle=False, 
+                                                    num_workers=int(eva.opt.workers))
+
+    print('number of test samples is: {0} % {1} times'.format(len(test_video_dataset),iter))
+    print('finished loading test data % {0} times'.format(iter))
+
+    if eva.opt.init_model != '':
+        print('loading pretrained model from {0}'.format(eva.opt.init_model))
+        model.load_state_dict(torch.load(eva.opt.init_model))
+
+    if eva.opt.cuda:
+        print('shift model to GPU .. ')
+        model = model.cuda()            
+    
+    evaluate.test(test_video_loader, test_audio_loader, model, opt)
